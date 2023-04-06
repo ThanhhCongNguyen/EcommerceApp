88cac0622b32aadd54c8c87cebfbf953df03cf2a
ThanhNC <nguyencongthanh02@vccorp.vn>
2023-04-06 17:51:37 +0700

    update

--- app/src/main/java/com/example/ecommerceapp/ui/CustomDialogClass.java
+++ app/src/main/java/com/example/ecommerceapp/ui/CustomDialogClass.java
@@ -1,13 +1,33 @@
 package com.example.ecommerceapp.ui;
 
+import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.core.app.DialogCompat;
+import androidx.fragment.app.DialogFragment;
 
-public class CustomDialogClass extends Dialog {
-    public CustomDialogClass(@NonNull Context context) {
-        super(context);
+public class CustomDialogClass extends DialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the Builder class for convenient dialog construction
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setMessage(R.string.r)
+                .setPositiveButton(R.string.start, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // START THE GAME!
+                    }
+                })
+                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User cancelled the dialog
+                    }
+                });
+        // Create the AlertDialog object and return it
+        return builder.create();
     }
+
 }
